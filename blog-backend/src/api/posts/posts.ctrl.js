93dee174367abf311e75e1cbb489ea3da32afc28
이정민 <leejm@ddhinc.net>
2021-12-30 17:52:10 +0900

    feat: add mongoDB & add post rest api (#2)

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,107 +1,120 @@
-const { post } = require(".");
+import Post from "../../models/post";
+import mongoose from "mongoose";
+import Joi from "joi";
 
-let postId = 1;
+const { ObjectId } = mongoose.Types;
 
-// post 배열 초기 데이터
-const posts = [
-    {
-        id: 1,
-        title: '제목',
-        body: '내용',
-    },
-];
-
-/* 포스트 작성
-POST /api/posts
-{title, body}
-*/
-export const write = ctx => {
-    const { title, body } = ctx.request.body;
-    postId += 1;
-    const post = {id: postId, title, body};
-    posts.push(post);
-    ctx.body = post;
+export const checkObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  return next();
 };
 
-/* 포스트 목록 조회 
-GET /api/posts
-*/
-export const list = ctx => {
+export const write = async (ctx) => {
+  const schema = Joi.object().keys({
+    title: Joi.string().required(),
+    body: Joi.string().required(),
+    tags: Joi.array().items(Joi.string()).required(),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  const { title, body, tags } = ctx.request.body;
+  const post = new Post({
+    title,
+    body,
+    tags,
+  });
+  try {
+    await post.save();
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+    s;
+  }
+};
+
+export const list = async (ctx) => {
+  const page = parseInt(ctx.query.page || "1", 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
+  try {
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(15)
+      .skip((page - 1) * 15)
+      .exec();
+      const postCount = await Post.countDocuments().exec();
+      ctx.set('Last-Page', Math.ceil(postCount/15));
+      ctx.body = posts.map(post => post.toJSON()).map(post => ({
+          ...post,
+          body: post.body.legnth< 200 ? post.body: `${post.body.slice(0,200)}...`,
+      }))
     ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
-
-/* 특정 포스트 조회
-GET /api/posts/:id
-*/
-export const read =ctx =>{
-    const {id} = ctx.params;
-    const post = posts.find(p => p.id.toString() === id);
-    if(!post) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
     }
     ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
-
-/* 특정 post 제거
-DELETE /api/posts/:id
-*/
-export const remove = ctx =>{
-    const { id } = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body ={
-            message :'포스트가 존재하지 않습니다.',
-        };
-        return;
-    }
-
-    // index 번째 아이템을 제서
-    posts.slice(index,1);
+export const remove = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
     ctx.status = 204;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
+export const update = async (ctx) => {
+  const { id } = ctx.params;
 
-/* 포스트 수정 
-PUT /api/posts/:id
-*/
-export const replace = ctx => {
-    const {id} = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if(index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message : '포스트가 존재하지 않습니다.',
-        };
-        return;
+  const schema = Joi.object().keys({
+    title: Joi.string(),
+    body: Joi.string(),
+    tags: Joi.array().items(Joi.string()),
+  });
+
+  const result = schema.validate(ctx.request.body);
+  if (result.error) {
+    ctx.status = 400;
+    ctx.body = result.error;
+    return;
+  }
+
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true,
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
     }
-    posts[index] = {
-        id,
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
-
-/* 포스트 수정(특정 필드 변경)
-PATCH /api/posts/:id
-{title, body}
-*/
-export const update = ctx =>{
-    const {id} = ctx.params;
-    const index = posts.findIndex(p => p.id.toString() === id);
-    if( index === -1) {
-        ctx.status = 404;
-        ctx.body = {
-            message: '포스트가 존재하지 않습니다.',
-        };
-        return;
-    }
-    posts[index]= {
-        ...posts[index],
-        ...ctx.request.body,
-    };
-    ctx.body = posts[index];
-}
--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -1,25 +1,26 @@
-require('dotenv').config();
-import Koa from 'koa';
-import Router from 'koa-router';
-import bodyParser from 'koa-bodyparser';
-import mongoose  from 'mongoose';
+require("dotenv").config();
+import Koa from "koa";
+import Router from "koa-router";
+import bodyParser from "koa-bodyparser";
+import mongoose from "mongoose";
 
-import api from './api';
+import api from "./api";
 
-const {PORT, MONGO_URI} = process.env;
+const { PORT, MONGO_URI } = process.env;
 
 const app = new Koa();
 const router = new Router();
 
-mongoose.connect(MONGO_URI, {useNewUrlParser: true })
-.then(() => {
-console.log('Connect to MongoDB');
-})
-.catch(e => {
+mongoose
+  .connect(MONGO_URI, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connect to MongoDB");
+  })
+  .catch((e) => {
     console.error(e);
-});
-// 라우터 설정 
-router.use('/api', api.routes());
+  });
+// 라우터 설정
+router.use("/api", api.routes());
 
 // 라우터 적용 전에 bodyparser
 app.use(bodyParser());
@@ -29,5 +30,5 @@ app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
 app.listen(port, () => {
-    console.log('Listening to port %d', port);
-})
+  console.log("Listening to port %d", port);
+});
