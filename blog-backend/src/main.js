8ee2b3b025d0d19b4c7e410d92df56e4aa7ffe4d
이정민 <leejm@ddhinc.net>
2022-01-06 09:58:46 +0900

    feat: delete comment

--- blog-backend/src/main.js
+++ blog-backend/src/main.js
@@ -19,13 +19,11 @@ mongoose
   .catch((e) => {
     console.error(e);
   });
-// 라우터 설정
+
 router.use("/api", api.routes());
 
-// 라우터 적용 전에 bodyparser
 app.use(bodyParser());
 
-// app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
 
 const port = PORT || 4000;
